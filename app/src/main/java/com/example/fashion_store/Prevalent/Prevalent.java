c5d7b075e951034c43c839a3ccbd3de6e93f5ad5
Sithum-Ravishka <79629419+Sithum-Ravishka@users.noreply.github.com>
2021-08-04 17:56:35 +0530

    Successful Create Login and Registration.

---
+++ app/src/main/java/com/example/fashion_store/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.fashion_store.Prevalent;
+
+import com.example.fashion_store.Model.Users;
+
+public class Prevalent {
+    private static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
